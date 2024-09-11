81b7dd902402b86525af9c58f7aa894b326e52d6
coffee <hyiz0224@gmail.com>
2024-09-11 16:44:14 +0800

    add param to shell

--- job.sh
+++ job.sh
@@ -13,7 +13,22 @@ module load compiler/dtk/24.04
 
 module list
 
-make clean 
 make -j
+preliminary_1="64 256 14 14 256 3 3 1 1 1 1"
+preliminary_2="256 192 14 14 192 3 3 1 1 1 1"
+preliminary_3="16 256 26 26 512 3 3 1 1 1 1"
+preliminary_4="32 256 14 14 256 3 3 1 1 1 1"
+preliminary_5="2 1280 16 16 1280 3 3 1 1 1 1"
+preliminary_6="2 960 64 64 32 3 3 1 1 1 1"
 
-srun ./build/conv2ddemo 64 256 14 14 256 3 3 1 1 1 1
+final_1="16 128 64 64 27 3 3 1 1 1 1"
+final_2="16 256 32 32 256 3 3 1 1 1 1"
+final_3="16 64 128 128 64 3 3 1 1 1 1"
+final_4="2 1920 32 32 640 3 3 1 1 1 1"
+final_5="2 640 64 64 640 3 3 1 1 1 1"
+final_6="2 320 64 64 4 3 3 1 1 1 1"
+
+param=$preliminary_1
+# hipprof -o ./prof/$TIMESTAMP.csv -d ./prof --hip-trace ./build/conv2ddemo 64 256 14 14 256 3 3 1 1 1 1
+echo "prof with parameter " $param
+srun ./build/conv2ddemo $param
